64889eaf5ebd7f29e70a3190fa462c44d8197fbd
Thorvald Jørgensen <thorvald@basahl.dk>
2020-03-31 15:12:59 +0200

    Still working on estimation

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ import sys
 from scipy.io.matlab import loadmat
 from sktensor import dtensor
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 import TensorHelpers
 import DTWHelpers
@@ -90,10 +91,10 @@ if True:
     # plt.plot(DTWHelpers.getSyntheticGraph(9), c='g')
     # plt.plot(DTWHelpers.getSyntheticGraph(0), c='r')
     # plt.show()
-
-    newSeq = subSeqList.pop(0)
-    newSeqLabel = labelsStacked[0]
-    labelsStacked = np.delete(labelsStacked,0).reshape([119,1])
+    currentlyTestingId = 50
+    newSeq = subSeqList[currentlyTestingId]
+    newSeqLabel = labelsStacked[currentlyTestingId]
+    # labelsStacked = np.delete(labelsStacked,0).reshape([119,1])
 
     seqsTrain, seqsTest, labelsTrain, labelsTest = train_test_split(subSeqList, labelsStacked, test_size = 0.20)
 
@@ -296,47 +297,88 @@ if SVM_classifier:
         mean_body[i] = np.resize(x, tensor.shape[2])
 
     print("U2 mean shape: ",np.mean(U2, axis=0).shape)
-    f_hatU3 = lambda u2,u3 : np.add(np.tensordot(np.tensordot(np.tensordot(core_S, U1, (0,1)), u2, (0,0)), u3, (0,0)), mean_new_shape)
     rand_U2 = np.random.rand(U2.shape[0])
     rand_U3 = np.random.rand(U3.shape[0],U3.shape[1])
     import concurrent.futures
-    for g, x in enumerate(stepSeqs):
+
+    f_hatU2 = lambda u2,U3 : np.add(np.tensordot(np.tensordot(np.tensordot(core_S, U1, (0,1)), u2, (0,0)), U3, (0,1)), mean_body)
+    f_hatU3 = lambda u2,u3 : np.add(np.tensordot(np.tensordot(np.tensordot(core_S, U1, (0,1)), u3, (1,0)), u2, (0,0)), mean_new_shape)
+    
+    # EstU3 = U3
+    # M3 = np.transpose(np.tensordot(np.tensordot(core_S, U1, (0,1)), U2[currentlyTestingId],(0,0)))
+    # M3pinv = np.linalg.pinv(M3)
+    # for i, x in enumerate(EstU3):
+    #     EstU3[i] = np.matmul(M3pinv,np.subtract(f_hatU3(U2[currentlyTestingId],x), mean_new_shape))
+            
+    # M2List = []
+    # for i, x in enumerate(U3):
+    #     M2List.append(np.transpose(np.tensordot(np.tensordot(core_S, U1, (0,1)),x,(1,0))))
+    # M2 = None
+    # for i, x in enumerate(M2List):
+    #     if M2 is None:
+    #         M2 = x
+    #     else:
+    #         M2 = np.vstack((M2, x))
+    # f_hatList = []
+    # for i, x in enumerate(U3):
+    #     f_hatList.append((np.subtract(f_hatU3(U2[currentlyTestingId],x), mean_new_shape)).reshape(45,1))
+    # f_hat_matrix = None
+    # for i, x in enumerate(f_hatList):
+    #     if f_hat_matrix is None:
+    #         f_hat_matrix = x
+    #     else:
+    #         f_hat_matrix = np.vstack((f_hat_matrix, x))
+    # print(M2.shape)
+    # print(f_hat_matrix.shape)
+    # print(np.tensordot(core_S, U1, (0,1)).shape)
+    # EstU2 = np.matmul(np.linalg.pinv(M2),f_hat_matrix).reshape(U2.shape[0])
+
+    # print("Difference in U3", np.linalg.norm(U3 - EstU3))
+    # print("Difference in U2", np.linalg.norm(U2[currentlyTestingId] - EstU2))
+    print(np.min(U2))
+    print(np.max(U2))
+    for g, seq in enumerate(stepSeqs):
         init_U2 = np.mean(U2, axis=0)
-        u2_hat = rand_U2
+        u2_hat = np.random.uniform(np.min(U2), high=np.max(U2), size=U2.shape[0])#rand_U2
         init_U3 = np.mean(U3, axis=0)
-        U3_hat = rand_U3
-        U2_list = np.zeros([stepSeqs.shape[0],u2_hat.shape[0]])
-        def minimize_U3(id):
-            opt_funU3 = lambda u3: 0.5 * np.abs(np.linalg.norm(f_hatU3(u2_hat,u3) - x[:,id]))**2
-            return (id,opti.minimize(opt_funU3, U3_hat[id,:], method='SLSQP', jac=rosen_der, options={'maxiter':30}).x)#, constraints = ({'type': 'eq', 'fun': lambda x: x.sum() - 1.0, 'jac': lambda x: np.ones_like(x)})).x)
-        for j in range (0,5):
+        U3_hat = np.random.uniform(np.min(U3), high=np.max(U3), size=(U3.shape[0],U3.shape[1]))#rand_U3
+        U2_list = []
+        errors = []
+        # def minimize_U3(id):
+        #     opt_funU3 = lambda u3: 0.5 * np.abs(np.linalg.norm(f_hatU3(u2_hat,u3) - seq[:,id]))**2
+        #     return (id,opti.minimize(opt_funU3, U3_hat[id,:], method='SLSQP', jac=rosen_der, options={'maxiter':30}).x)#, constraints = ({'type': 'eq', 'fun': lambda x: x.sum() - 1.0, 'jac': lambda x: np.ones_like(x)})).x)
+        for j in range (0,20):
             start = datetime.now()
-            with concurrent.futures.ThreadPoolExecutor(max_workers=15) as executor:
-                fibSubmit = {executor.submit(minimize_U3, n): n for n in range(0, x.shape[1])}
+            # if j == 0:
+            #     with concurrent.futures.ThreadPoolExecutor(max_workers=15) as executor:
+            #         fibSubmit = {executor.submit(minimize_U3, n): n for n in range(0, seq.shape[1])}
 
-                for future in concurrent.futures.as_completed(fibSubmit):
-                    try:
-                        n, f = future.result()
-                    except Exception as exc:
-                        print("Erro! {0}".format(exc))
-                    else:
-                        U3_hat[n] = f
-            # with concurrent.futures.ThreadPoolExecutor() as executor:
-            #     for i, out1 in enumerate(executor.map(minimize_U3, range(0, x.shape[1]))):
-            #         U3_hat[i] = out1
-            end = datetime.now()
+            #         for future in concurrent.futures.as_completed(fibSubmit):
+            #             try:
+            #                 n, f = future.result()
+            #             except Exception as exc:
+            #                 print("Error! {0}".format(exc))
+            #             else:
+            #                 U3_hat[n] = f
+                # with concurrent.futures.ThreadPoolExecutor() as executor:
+                #     for i, out1 in enumerate(executor.map(minimize_U3, range(0, x.shape[1]))):
+                #         U3_hat[i] = out1
+                # for i in range (0,x.shape[1]):
+                #     opt_funU3 = lambda u3: 0.5 * np.abs(np.linalg.norm(f_hatU3(u2_hat,u3) - x[:,i]))**2
+                #     # print(opt_fun(U3[i,:]))
+                #     U3_hat[i] = opti.minimize(opt_funU3, U3_hat[i,:], method='Nelder-Mead', options={'maxiter':15}).x
 
-            print("Time taken to estimate U3: ", end-start)
-            # for i in range (0,x.shape[1]):
-            #     opt_funU3 = lambda u3: 0.5 * np.abs(np.linalg.norm(f_hatU3(u2_hat,u3) - x[:,i]))**2
-            #     # print(opt_fun(U3[i,:]))
-            #     U3_hat[i] = opti.minimize(opt_funU3, U3_hat[i,:], method='Nelder-Mead', options={'maxiter':15}).x
-    # Continue from here:
-    #   Currently need to stack following results (Eq. 22)
-    #   Repeat untill convergence
+
+            
+            M3 = np.transpose(np.tensordot(np.tensordot(core_S, U1, (0,1)), u2_hat,(0,0)))
+            M3pinv = np.linalg.pinv(M3)
+            for i, x in enumerate(U3_hat):
+                U3_hat[i] = np.matmul(M3pinv,np.subtract(f_hatU3(u2_hat,x), mean_new_shape))
+                
+
             M2List = []
             for i, x in enumerate(U3_hat):
-                M2List.append(np.transpose(np.tensordot(np.tensordot(core_S, U1, (0,0)),x,(1,0))))
+                M2List.append(np.transpose(np.tensordot(np.tensordot(core_S, U1, (0,1)),x,(1,0))))
             M2 = None
             for i, x in enumerate(M2List):
                 if M2 is None:
@@ -345,31 +387,107 @@ if SVM_classifier:
                     M2 = np.vstack((M2, x))
             f_hatList = []
             for i, x in enumerate(U3_hat):
-                f_hatList.append((f_hatU3(u2_hat,x)- mean_new_shape).reshape(45,1))
+                f_hatList.append((np.subtract(f_hatU3(u2_hat,x), mean_new_shape)).reshape(45,1))
             f_hat_matrix = None
             for i, x in enumerate(f_hatList):
                 if f_hat_matrix is None:
                     f_hat_matrix = x
                 else:
                     f_hat_matrix = np.vstack((f_hat_matrix, x))
-            print(f_hat_matrix.shape)
             u2_hat = np.matmul(np.linalg.pinv(M2),f_hat_matrix).reshape(U2.shape[0])
             #u2_hat = np.mean(u2_hat, axis=0)
-            print("u2 hat shape : ",u2_hat.shape)
+            end = datetime.now()
+            errors.append(0.5 * np.abs(np.linalg.norm(f_hatU2(u2_hat,U3_hat) - seq))**2)
+            U2_list.append(u2_hat)
 
-    # f_hatU2 = lambda u2,U3 : np.add(np.tensordot(np.tensordot(np.tensordot(core_S, U1, (0,0)), u2, (0,0)), U3, (0,0)), mean_body)
+            print("M2 condition", np.linalg.cond(M2))
+            print("M3 condition", np.linalg.cond(M3))
+            print("U3_hat condition", np.linalg.cond(U3_hat))
+            print("U2 condition", np.linalg.cond(U2))
+            print("U3 condition", np.linalg.cond(U3))
+            print("Test condition", np.linalg.cond(np.add(np.tensordot(np.tensordot(np.tensordot(core_S, U1, (0,1)), U2[currentlyTestingId], (0,0)), U3, (0,1)), mean_body)))
+            print("Iteration:", j, " Time taken to estimate: ", end-start)
+
+        newMatrix1 = np.tensordot(core_S, U1, (0,1))
+        newMatrix2 = np.tensordot(newMatrix1,u2_hat, (0,0))
+        newMatrix = np.tensordot(newMatrix2,U3_hat,(0,1))
+        FirstFrameModel = np.add(newMatrix, mean_body)
+
+        xs = []
+        ys = []
+        zs = []
+
+        # Split the data into x,y,z coordinates for each frame
+        for frm in range(0, FirstFrameModel.shape[1]):
+            for j in range(0, 45, 3):
+                xs.append(FirstFrameModel[j][frm])
+                ys.append(FirstFrameModel[j+1][frm])
+                zs.append(FirstFrameModel[j+2][frm])
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        sct, = ax.plot([], [], [], "o", markersize=2)
+
+        # Update function to be called each frame
+        def update(ifrm, xa, ya, za):
+            # Clear all lines (except points)
+            ax.lines = [ax.lines[0]]
+
+            sct.set_data(xa[ifrm*15:ifrm*15+14], ya[ifrm*15:ifrm*15+14])
+            sct.set_3d_properties(za[ifrm*15:ifrm*15+14])
+            # For drawing the lines between points
+            # Right leg
+            ax.plot3D([xa[ifrm*15+0], xa[ifrm*15+1]], [ya[ifrm*15+0], ya[ifrm*15+1]], [za[ifrm*15+0], za[ifrm*15+1]], 'steelblue', markersize=2)
+            ax.plot3D([xa[ifrm*15+1], xa[ifrm*15+2]], [ya[ifrm*15+1], ya[ifrm*15+2]], [za[ifrm*15+1], za[ifrm*15+2]], 'steelblue', markersize=2)
+            ax.plot3D([xa[ifrm*15+2], xa[ifrm*15+3]], [ya[ifrm*15+2], ya[ifrm*15+3]], [za[ifrm*15+2], za[ifrm*15+3]], 'steelblue', markersize=2)
+            # Left leg
+            ax.plot3D([xa[ifrm*15+0], xa[ifrm*15+4]], [ya[ifrm*15+0], ya[ifrm*15+4]], [za[ifrm*15+0], za[ifrm*15+4]], 'steelblue', markersize=2)
+            ax.plot3D([xa[ifrm*15+4], xa[ifrm*15+5]], [ya[ifrm*15+4], ya[ifrm*15+5]], [za[ifrm*15+4], za[ifrm*15+5]], 'steelblue', markersize=2)
+            ax.plot3D([xa[ifrm*15+5], xa[ifrm*15+6]], [ya[ifrm*15+5], ya[ifrm*15+6]], [za[ifrm*15+5], za[ifrm*15+6]], 'steelblue', markersize=2)
+            # Spine
+            ax.plot3D([xa[ifrm*15+0], xa[ifrm*15+7]], [ya[ifrm*15+0], ya[ifrm*15+7]], [za[ifrm*15+0], za[ifrm*15+7]], 'steelblue', markersize=2)
+            ax.plot3D([xa[ifrm*15+7], xa[ifrm*15+8]], [ya[ifrm*15+7], ya[ifrm*15+8]], [za[ifrm*15+7], za[ifrm*15+8]], 'steelblue', markersize=2)
+            # Right arm
+            ax.plot3D([xa[ifrm*15+7], xa[ifrm*15+9]], [ya[ifrm*15+7], ya[ifrm*15+9]], [za[ifrm*15+7], za[ifrm*15+9]], 'steelblue', markersize=2)
+            ax.plot3D([xa[ifrm*15+9], xa[ifrm*15+10]], [ya[ifrm*15+9], ya[ifrm*15+10]], [za[ifrm*15+9], za[ifrm*15+10]], 'steelblue', markersize=2)
+            ax.plot3D([xa[ifrm*15+10], xa[ifrm*15+11]], [ya[ifrm*15+10], ya[ifrm*15+11]], [za[ifrm*15+10], za[ifrm*15+11]], 'steelblue', markersize=2)
+            # Left arm
+            ax.plot3D([xa[ifrm*15+7], xa[ifrm*15+12]], [ya[ifrm*15+7], ya[ifrm*15+12]], [za[ifrm*15+7], za[ifrm*15+12]], 'steelblue', markersize=2)
+            ax.plot3D([xa[ifrm*15+12], xa[ifrm*15+13]], [ya[ifrm*15+12], ya[ifrm*15+13]], [za[ifrm*15+12], za[ifrm*15+13]], 'steelblue', markersize=2)
+            ax.plot3D([xa[ifrm*15+13], xa[ifrm*15+14]], [ya[ifrm*15+13], ya[ifrm*15+14]], [za[ifrm*15+13], za[ifrm*15+14]], 'steelblue', markersize=2)
+
+        # Limit coordinates for all axes
+        ax.set_xlim(30,-30)
+        ax.set_ylim(30,-30)
+        ax.set_zlim(-30,30)
+
+        # Set labels
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.set_zlabel("z")
+
+        # Run animation with the update function and point lists
+        ani = animation.FuncAnimation(fig, update, FirstFrameModel.shape[1], fargs=(xs,ys,zs), interval=200)
+        plt.show()
+
     # opt_funU2 = lambda u2: 0.5 * np.abs(np.linalg.norm(f_hatU2(u2,U3_hat) - x))**2
     # u2_hat = opti.minimize(opt_funU2, u2_hat, method='Newton-CG', jac=rosen_der, options={'maxiter':10}).x
     # U2_list[g,:] = u2_hat
     
-        print(U2.shape)
         #print(np.array(u2_hat).shape)
         print("Coordinates of U2: ", u2_hat.reshape(1,U2.shape[0])[0,:3])
-        Plotting.plotU2(np.vstack((U2, u2_hat.reshape(1,U2.shape[0]))), np.vstack((labelsStacked, [13])), np.append(action_names, 'Test'))
-        print(newSeqLabel)
+        labelsStacked[currentlyTestingId] = [14]
+        newU2 = U2
+        newLabels = labelsStacked
+        for x in U2_list:
+            newU2 = np.vstack((newU2, x.reshape(1,U2.shape[0])))
+            newLabels = np.vstack((newLabels, [13]))
+        Plotting.plotU2(newU2, newLabels, np.append(np.append(action_names, 'Test'), 'Original'))
+        fig = plt.figure()
+        ax = plt.axes()
+        ax.plot(errors)
+
         plt.show()
-        print(u2_hat.shape)
-        print(U3_hat.shape)
     # opt_fun = lambda u2,u3: f_hat(u2,u3) - 
     sys.exit()
 
